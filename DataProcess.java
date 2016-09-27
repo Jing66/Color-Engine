38f88ec484d89ccdd0ae799589a64a2899842a27
Jing <liujingyun0209@gmail.com>
2016-09-27 15:19:52 -0400

    install Apache Poi to class path

--- DataProcess.java
+++ DataProcess.java
@@ -1,20 +1,42 @@
 package colors;
+//Apache Poi library
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.io.File;
+//JDK Library
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 /**
  * process data: get Var from excel, get expectation from Bloomberg, get real-time data from 
  * 				Bloomberg
  * @author liujingyun
  *
  */
+
+
 public class DataProcess {
 
 	public DataProcess() {
 		// TODO Auto-generated constructor stub
 	}
 	
+	//************Read variable from the H4 cell********/
 	public static int getVar(String fileName){
 		int var=0;
+		try{
+			FileInputStream file = new FileInputStream(new File("filename"));
+		}
+		catch(FileNotFoundException e){
+			System.out.print("File not found");
+		}
 		
 		return var;
 	}
 
+	//*********** TESTING ****************************/
+	public static void main(String[] args){
+		
+	}
 }
