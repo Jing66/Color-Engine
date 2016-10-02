1286a72323dafd859a5b84e1019e5d0c5cfed4db
Jing <liujingyun0209@gmail.com>
2016-10-02 18:19:28 -0400

    auto generate table cells

--- Bars.java
+++ Bars.java
@@ -7,8 +7,12 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -17,6 +21,7 @@ import java.util.Hashtable;
 
 import javax.swing.JLabel;
 import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
@@ -38,8 +43,9 @@ public class Bars extends JFrame {
 				try {
 					Hashtable<String,Integer> test = new Hashtable<String,Integer>();
 					test.put("a",1);
-					test.put("b",0);test.put("c",0);
+					test.put("b",0);test.put("c",0);test.put("d",0);test.put("e",0);test.put("f",0);
 					Bars frame = new Bars(test);
+					//frame.pack();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -55,7 +61,7 @@ public class Bars extends JFrame {
 		setTitle("Bars Test");
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 750, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(8, 10, 8, 10));
 		contentPane.setLayout(new BorderLayout(5, 10));
@@ -70,64 +76,58 @@ public class Bars extends JFrame {
 		//TODO: make grids and empty bars
 		setTitle("Bars Ready");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 750, 600);
+		setBounds(100, 100, 950, 600);
 		contentPane = new JPanel();
 		
 		contentPane.setBorder(new EmptyBorder(8, 10, 8, 10));
-		contentPane.setLayout(new BorderLayout(5, 10));
+		//contentPane.setLayout(new BorderLayout(5, 10));
 		setContentPane(contentPane);
-		//grid layout
-		GridLayout test = new GridLayout((choices.size())+2,1);
-		getContentPane().setLayout(test);
+		//++++++++++++++++++Layout++++++++++++++++++++++++++++++++
+		//GridLayout test = new GridLayout((choices.size())+2,1);
+		//getContentPane().setLayout(test);
+		BoxLayout boxlayout = new BoxLayout(contentPane, BoxLayout.PAGE_AXIS);  //Y_AXIS: vertical
+		contentPane.setLayout(boxlayout);
+		
 		Enumeration<String> keys = choices.keys();
+		int count = 0;
 		while (keys.hasMoreElements()){
-			
+			count++;
 			String i = keys.nextElement();
-			PanelBars barPane = new PanelBars(i);
-			
-			getContentPane().add(barPane);
-			contentPane.add(Box.createRigidArea(new Dimension(10,10)));
-			
+			//Draw Rectangle
+			RectDraw rect = new RectDraw(i,count,choices.get(i));
+			String text = "<html><B>"+i+"</B> </html>";
+			JLabel indicator = new JLabel(text);
+			indicator.setFont(new Font("Serif", Font.PLAIN, 20));
+			contentPane.add(indicator);
+			contentPane.add(rect);
 		}
 		
 	}
 	
-	/**********************PanelBar Class***********************/
-	class PanelBars extends JPanel{
-		private JPanel pane;
+	/**********************Draw Rectangle***********************/
+	private static class RectDraw extends JPanel{
+		//BOND = 0; INVERSE = 1;
+		private String name;
+		private int bond; 
+		private int order;
+	//	private Rectangle box;
 		
-		public PanelBars(String i){
-			pane = new JPanel();
-			SpringLayout layout = new SpringLayout();
-			
-			setLayout(layout);
-			//Add label
-			JLabel label = new JLabel(i);
-			//Add table
-			DefaultTableModel model = new DefaultTableModel(1,50);
-			
-			JTable table = new JTable(model);
-			layout.putConstraint(SpringLayout.WEST, label, 5, SpringLayout.EAST,pane);
-			setLayout(layout);
-			
-			add(label);
-			add(table);
-			
+		public RectDraw(String name, int order, int bond){
+			setPreferredSize(new Dimension(600,20));
+			this.name = name;
+			this.order = order;
+			this.bond = bond;
 		}
 		
-	}
-	
-	/**********************TableBar Class***********************/
-	class BarTable extends JTable{
-		private JTable table;
-		
-		public BarTable(){
-			DefaultTableModel model = new DefaultTableModel(1,50);
-			JTable table = new JTable(model);
-			this.table=table;
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);  
+			for (int start = 100;start<900;start+=25){
+		       	 g.drawRect(start,order*4,25,30);
+		    }
+			g.setFont(new Font("default", Font.BOLD, 16));
+		    //g.drawString(name, 500, order*10);	
 		}
 	}
-
 	
 	
 }
--- TestPane.java
+++ TestPane.java
@@ -1,13 +1,17 @@
 package colors;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.LayoutManager;
+import java.awt.Rectangle;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -17,11 +21,13 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class TestPane extends JFrame  {
 
 	private JPanel contentPane;
-
+	private Rectangle box;
 	/**
 	 * Launch the application.
 	 */
@@ -44,11 +50,11 @@ public class TestPane extends JFrame  {
 	 */
 	public TestPane() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 750, 600);
 		contentPane = new JPanel();
 		//contentPane.setBorder(new EmptyBorder(new Insets(150, 200, 150, 200)));
 		//+++++++++++++++++++add BoxLayout+++++++++++++++++
-		BoxLayout boxlayout = new BoxLayout(contentPane, BoxLayout.PAGE_AXIS);  //Y_AXIS: vertical
+		BoxLayout boxlayout = new BoxLayout(contentPane, BoxLayout.Y_AXIS);  //Y_AXIS: vertical
 		contentPane.setLayout(boxlayout);
 		
 		//+++++++++++++++++++add GridBagLayout+++++++++++++++
@@ -75,11 +81,68 @@ public class TestPane extends JFrame  {
 		contentPane.add(Box.createRigidArea(new Dimension(0, 10))); //Create space between 2 components
 		contentPane.add(label2);
 		contentPane.add(label3);
-		contentPane.add(Box.createHorizontalGlue());  //Another way to create space
+		//contentPane.add(Box.createHorizontalGlue());  //Another way to create space
 		contentPane.add(button1);
+		
+		contentPane.add(new JLabel("NFP"));
+		RectDraw rect = new RectDraw();
+		//QUESTION: how to add listener?????
+		
+		contentPane.add(rect);
+		contentPane.add(new JLabel("ISM"));	//NOTE: it's far away.
+		
+		
+		
 		//++++++++++++++++++++++++++Scroll panel++++++++++++++++++++
 		JScrollPane scrollPane = new JScrollPane(contentPane);
 		setContentPane(scrollPane);
 	}
+	
+	/***********************Events Listener***********************/
+	class BooleanListener implements ChangeListener{
+
+		@Override
+		public void stateChanged(ChangeEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	private static class RectDraw extends JPanel {
+        public void paintComponent(Graphics g) {
+        super.paintComponent(g);  
+        
+/*         g.drawRect(100,50,100,20);  	//args: (距离左边，距离上面,length,width)  
+         //g.fillRect(130,50,300,20);  //fill it
+         g.drawRect(200, 50, 100, 20);  //connect: x' = x + length
+         g.drawRect(300, 50, 100, 20);  //connect: x' = x + length
+         g.drawRect(230,120,100,20);  //draw a hollow rectangle
+         g.setColor(Color.RED);	//set the border to be red
+*/
+        //generate 20 black rectangles connected together
+        for (int start = 100;start<600;start+=25){
+       	 g.drawRect(start,10,25,30);
+        }
+        g.drawString("150K", 350, 10);
+        g.setColor(Color.BLACK);
+        
+        //Event on certain rectangle
+        Rectangle box = new Rectangle(5,10,20,30);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.draw(box);
+        
+        //Simulate a change in data
+        boolean change = false;
+        try {
+			Thread.sleep(2000);
+			change = true;
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+        
+        if(change) {g.fillRect(5,10,20,30); g.drawString("TestNumber", 350, 25);}
+        }
+     }
 
 }
