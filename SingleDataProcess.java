4aee83df79e874899c40b89086d3e4fde11ec0a0
Jing <liujingyun0209@gmail.com>
2016-11-10 10:48:31 -0500

    Initialize single data process

---
+++ SingleDataProcess.java
@@ -0,0 +1,28 @@
+package colors;
+
+import java.util.ArrayList;
+
+import javax.swing.SwingWorker;
+
+public class SingleDataProcess extends SwingWorker<Double,Void>{
+	public Bars bar;
+	
+	
+	
+	public SingleDataProcess() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected Double doInBackground() throws Exception {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+	
+	@Override
+	protected void done(){
+		
+	}
+	
+}
